18088b71bf09b0ed65c94ea9b634b74a5bdd8fd0
Dima <address>
2021-02-08 23:42:55 +0300

    X

--- server.js
+++ server.js
@@ -16,11 +16,14 @@ connectDB();
 app.use('/api/v1/transactions', transactionRoutes)
 
 if (process.env.NODE_MODE === 'production') {
-    app.use(express.static('client/build'))
+
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+
 }
 
 
-app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 
 
 const PORT = process.env.PORT || 5000
